f2be4b853d051a2f8c866edbbedfacab5f345f72
dumas <dz.ma@shunwang.com>
2022-02-16 14:07:02 +0800

    rpc-service-api 接口层

---
+++ rpc-service-api/src/main/java/com/dumas/rpc/Hello.java
@@ -0,0 +1,19 @@
+package com.dumas.rpc;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+/**
+ * @author dumas
+ * @date 2022/02/16 1:55 PM
+ */
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
+public class Hello implements Serializable {
+    private String message;
+    private String description;
+}
---
+++ rpc-service-api/src/main/java/com/dumas/rpc/HelloService.java
@@ -0,0 +1,9 @@
+package com.dumas.rpc;
+
+/**
+ * @author dumas
+ * @date 2022/02/16 1:57 PM
+ */
+public interface HelloService {
+    String hello(Hello hello);
+}
